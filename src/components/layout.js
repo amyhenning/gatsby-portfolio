603860cb53ba520dcd37585f349ad7860f632418
amyhenning <henning.amy@gmail.com>
2020-01-17 22:56:32 -0600

    Style index and responsive nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
 import styles from './layout.css'
 import Icofont from 'react-icofont'
 
-const ListLink = props => (
-	<li className="list-link">
-		<Link to={props.to}>{props.children}</Link>
-	</li>
-)
+let showMenu = false
+let activeClass
+
+
+
+function handleClick(e) {
+  e.preventDefault();
+  showMenu = !showMenu;
+  activeClass = showMenu == true ? 'active' : ''
+  console.log(showMenu);
+  console.log(activeClass);
+}
 
 export default ({children}) => (
-	<div id="layout-container">
-		<header>
-			<Link to="/" className="name-link">
-				<h1>Amy Henning</h1>
-			</Link>
+  <div id="layout-container">
+    <nav className="nav">
+      <ul className="menu">
+        <li className="logo"><a href="/"><h1>Amy Henning</h1></a></li>
+        <li className="item"><a href="/">Home</a></li>
+        <li className="item"><a href="/about">About</a></li>
+        <li className="item"><a href="/blog">Blog</a></li>
+        <li className="item"><a href="/work">Work</a></li>
+        <li className="item last"><a href="/contact">Contact</a></li>
+        <li className="toggle"><Icofont icon="icofont-navigation-menu" className="hamburger" onClick={handleClick} /></li>
+      </ul>
+    </nav>
 
-			<Link to="/" className="small-name-link">
-				<h1>Amy H</h1>
-			</Link>
+    {children}
 
-			<ul>
-				<ListLink to="/" className="list-link">Home</ListLink>
-				<ListLink to="/about/" className="list-link">About</ListLink>
-				<ListLink to="/blog/" className="list-link">Blog</ListLink>
-				<ListLink to="/work/" className="list-link">Work</ListLink>
-				<ListLink to="/contact/" className="list-link">Contact</ListLink>
-			</ul>
-		</header>
-
-		{children}
-
-		<footer>
-			Coded by Amy with Gatsby
-		</footer>
-	</div>
+    <footer className="footer">
+      Coded by Amy with Gatsby
+    </footer>
+  </div>
 )
