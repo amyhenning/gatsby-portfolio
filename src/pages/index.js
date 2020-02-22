c53172039e4b2e11c9b748c0fbc40ece04920f7d
amyhenning <henning.amy@gmail.com>
2020-02-22 14:05:39 -0600

    Add about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Icofont from 'react-icofont'
 import './index.css'
+
 export default () => (
 	<Layout>
-		<h1 className="intro">I'm a full-stack developer who loves Ruby <Icofont icon="icofont-file-ruby" />, learning <Icofont icon="icofont-learn" />, and problem-solving <Icofont icon="icofont-light-bulb" />.</h1>
-
+		<h1 className="intro">
+			I'm a full-stack developer who loves Ruby <Icofont icon="icofont-file-ruby" />,
+			learning <Icofont icon="icofont-learn" />,
+			and problem-solving <Icofont icon="icofont-light-bulb" />.
+		</h1>
 	</Layout>
 )
